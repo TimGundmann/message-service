a8912bf7fe9ccbe0dc19f433ef03574c78244bc5
Tim Gundmann <tim@gundmann.dk>
2019-02-24 19:47:51 +0100

    add method get category

--- src/main/java/dk/gundmann/plan/PlanController.java
+++ src/main/java/dk/gundmann/plan/PlanController.java
@@ -2,18 +2,13 @@ package dk.gundmann.plan;
 
 import java.time.LocalDate;
 
-import javax.transaction.Transactional;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Transactional
 public class PlanController {
 
 	private PlanRepository planRepository;
@@ -34,6 +29,10 @@ public class PlanController {
 		return categoryRepository.findAll();
 	}
 
+	@GetMapping("/categories/{name}")
+	public Category getCategorie(@PathVariable String name) {
+		return categoryRepository.findById(name).get();
+	}
 	
 	@GetMapping("/{category}/active")
 	public Iterable<Plan> getActivePlans(@PathVariable String category) {
